0f896df280c54b03a5baf81b86ef95149e03cbf2
hacktor <inf2project@gmail.com>
2022-01-10 18:37:41 +0800

    Update config file,and it have real function

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,58 @@
 //#![deny(warnings)]
 #![allow(non_snake_case)]
 #![allow(unused)]
+use std::fs;
 
 use warp::Filter;
 use warp::body::json;
 use warp::reply;
 
 use serde_json::json;
+use serde_json::{Result, Value};
+use serde::{Deserialize, Serialize};
 
-// struct Article{
-//     articleTitle: String,
-//     articleText: String,
-// }
-// struct ArticleList {
-//     articles: Vec<Article>,
-// }
+//use rusqlite::{params, Connection, Result as SqlResult};
+
+
+struct Article{
+    articleTitle: String,
+    articleText: String,
+}
+struct ArticleList {
+    articles: Vec<Article>,
+}
+
+#[derive(Clone)]
+#[derive(Serialize, Deserialize,Debug)]
+struct NavItem {
+    navItem: String,
+    navItemUrl: String,
+}
+
+#[derive(Clone)]
+#[derive(Serialize, Deserialize,Debug)]
+struct Config {
+    avatarUrl: String,
+    introduction: String,
+    navItems: Vec<NavItem>,
+    articleRootPath: String
+}
+
+//读取配置文件
+fn read_config() -> String{
+    let file_path = "./config.cfg";
+    let config = 
+        fs::read_to_string(&file_path)
+        .expect("Failed to read config file");
+    //println!("{}", config);
+    config
+}
 
 #[tokio::main]
-
 async fn main() {
+    let config:Config = serde_json::from_str(read_config().as_str()).unwrap();
     println!("Welcome to little guy's sever console!");
+    println!("{:?}", config);
     
     //FUNC展示主页
     //GET / -> Text
@@ -45,11 +78,11 @@ async fn main() {
                 "articles":[
                     {
                         "articleTitle": "tt1",
-                        "articleText": "text1",
+                        "articleText": "text1"
                     },
                     {
                         "articleTitle": "tt2",
-                        "articleText": "text2",
+                        "articleText": "text2"
                     }
                 ]
             });
@@ -58,12 +91,13 @@ async fn main() {
 
     //FUNC 获取个人简介
     //Get /getIntroduction -> Introduction
+    let introduction = config.introduction.clone();
     let get_introduction = 
         warp::path("getIntroduction")
-        .map(||{
+        .map(move||{
             let introduction = json!({
                 "introduction": {
-                    "introduction": "This is Little Guy's introduction",
+                    "introduction": introduction
                 }
             });
             warp::reply::json(&introduction)
@@ -71,33 +105,26 @@ async fn main() {
 
     //FUNC 获取头像
     //Get /getAvatarUrl -> AvataUrl
+    let avatar = config.avatarUrl.clone();
     let get_avatar = 
         warp::path("getAvatarUrl")
-        .map(||{
+        .map( move ||{
             let avatar = json!({
                 "avatar":{
-                    "avatar":"/images/avatar/avatar.jpg"
+                    "avatar":avatar
                 }
             });
             warp::reply::json(&avatar)
         });
-        
+
     //FUNC 获取导航栏条目与其绑定的URL
     //GET /getNavItems -> navItems
+    let navItems = config.navItems.clone();
     let get_navItems = 
         warp::path("getNavItems")
-        .map(||{
+        .map(move ||{
             let navItems = json!({
-                "navItems":[
-                    {
-                        "navItem":"item1",
-                        "navItemUrl":"url1",
-                    },
-                    {
-                        "navItem":"item2",
-                        "navItemUrl":"url2",
-                    },
-                ]
+                "navItems":navItems
             });
             warp::reply::json(&navItems)
         });
@@ -111,6 +138,6 @@ async fn main() {
         .or(get_navItems);
 
     warp::serve(routes)
-        .run(([0, 0, 0, 0], 80))
+        .run(([0, 0, 0, 0], 3031))
         .await;
 }
